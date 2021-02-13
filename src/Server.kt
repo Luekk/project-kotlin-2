4d5db9fb4d3f0d5f61f147533787a454a08834e3
Luekk <luck12345@interia.pl>
2021-02-13 14:39:22 +0100

    multiply server

--- src/Server.kt
+++ src/Server.kt
@@ -2,26 +2,27 @@ import java.lang.Thread.sleep
 
 class Server(override val processor: Processor, override val graphics: Graphics, override val ram: RAM): Computer() {
     fun req(a:Int,b:Int):Int{
-        println("Serwer odebrał zapytanie")
+        println("Serwer: Odebrano zapytanie")
         println(a)
         res(a,b)
         return a+b
     }
-    fun res(a:Int,b:Int){
-        println("Przetwarzanie zapytania")
+    private fun res(a:Int,b:Int){
         fun waiting():String {
             for (dot in 0..2) {
                 Thread.sleep(1000)
-                var dot = "."
-                print(dot)
+                print(".")
             }
+            println()
             return "end"
         }
+        fun multiply(a:Int,b:Int):Int {
+            return a * b
+        }
+        println("Przetwarzanie zapytania")
         val wait = waiting()
         if(wait == "end"){
-            val multiply = a*b
-            println()
-            println(multiply)
+            println(multiply(a,b))
         }
     }
 }
